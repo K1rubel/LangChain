0e394dead55b0ddf71f2a171bf0aa7b1f09ce519
Kiru Bel <110741421+K1rubel@users.noreply.github.com>
2024-11-27 15:47:19 +0300

    Add files via upload
    This is the simplest possible application of langchain framework. 🤷🏻

---
+++ simplest_lang_chain.py
@@ -0,0 +1,5 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+import sys
+llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key="AIzaSyC9qoe-Vue-9kN7RN5HTbHD0OrVXUur6_w")
+answer = llm.invoke("Tell me a joke!")
+print(f'{answer.content}\n')
